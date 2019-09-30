b24c90c3fc29cafd6849606dc81c30209a631927
David <davidpoarch@outlook.com>
2019-09-30 14:06:41 -0400

    fix error

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
-  style: ''
+  styleUrls: ['./plans.component.scss']
 })
-
 export class PlansComponent implements OnInit {
 
   constructor() { }
-
-
 
   ngOnInit() {
   }
