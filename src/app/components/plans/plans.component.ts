af2382b2994da3c99781ce5840d8edf503e64ea1
David <davidpoarch@outlook.com>
2019-09-30 13:50:42 -0400

    first commit

---
+++ src/app/components/plans/plans.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plans',
+  templateUrl: './plans.component.html',
+  style: ''
+})
+
+export class PlansComponent implements OnInit {
+
+  constructor() { }
+
+
+
+  ngOnInit() {
+  }
+
+}
